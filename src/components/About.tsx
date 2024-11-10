92b5f6e3d34c46ff0d899fead80dfea03b3a65d4
Faiza Anum <163395436+anumAsad@users.noreply.github.com>
2024-11-10 16:50:40 +0500

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -5,7 +5,7 @@ const About = () => {
     <div id='about' className='container pt-32'>
         <h2 className='text-4xl md:text-5xl' data-aos="zoom-out-down">About Me</h2>
         <p className='text-gray-500 pt-4' data-aos="zoom-out-down">
-        I am currently a student at the Government Institute of Artificial Intelligence and Computing (GIAIC), specializing in Artificial Intelligence and Web 3.0. Passionate about exploring AI technologies, I'm dedicated to developing innovative solutions that bridge the gap between technology and real-world applications.
+        I am currently a student at the Government Institute of Artificial Intelligence and Computing (GIAIC), specializing in Artificial Intelligence and Web 3.0. Passionate about exploring AI technologies, I am dedicated to developing innovative solutions that bridge the gap between technology and real-world applications.
         </p>
     </div>
   )
